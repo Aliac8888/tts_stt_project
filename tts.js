91311f979c46b823a71aaf7429b58f10933dc7d4
Aliac <alimehdizadeh822@gmail.com>
2025-02-02 16:33:00 +0330

    feat: fixed the deafult text to showcase tts properly

--- tts.js
+++ tts.js
@@ -5,15 +5,18 @@ function createOfflineTts() {
     // model: "./vits-persian/vits-piper-fa_IR-amir-medium/fa_IR-amir-medium.onnx",
     // tokens: "./vits-persian/vits-piper-fa_IR-amir-medium/tokens.txt",
     // dataDir: "./vits-persian/vits-piper-fa_IR-amir-medium/espeak-ng-data",
+
     // model: "./vits-persian/vits-piper-fa-haaniye_low/fa-haaniye_low.onnx",
     // tokens: "./vits-persian/vits-piper-fa-haaniye_low/tokens.txt",
     // dataDir: "./vits-persian/vits-piper-fa-haaniye_low/espeak-ng-data",
+
     model:
       "./vits-fa-en/vits-piper-fa_en-rezahedayatfar-ibrahimwalk-medium/fa_en-rezahedayatfar-ibrahimwalk-medium.onnx",
     tokens:
       "./vits-fa-en/vits-piper-fa_en-rezahedayatfar-ibrahimwalk-medium/tokens.txt",
     dataDir:
       "./vits-fa-en/vits-piper-fa_en-rezahedayatfar-ibrahimwalk-medium/espeak-ng-data",
+      
     // model: "./vits-en/vits-piper-en_US-amy-low/en_US-amy-low.onnx",
     // tokens: "./vits-en/vits-piper-en_US-amy-low/tokens.txt",
     // dataDir: "./vits-en/vits-piper-en_US-amy-low/espeak-ng-data",
@@ -40,7 +43,7 @@ const tts = createOfflineTts();
 const speakerId = 0;
 const speed = 1.0;
 const audio = tts.generate({
-  text: "درود بر استاد هامون مهدی زاده. این صدای تست متن به گفتار هست",
+  text: "درود بر استاد هامون مهدی زاده! این، صدای تست متن به گفتار است.",
   // text: "hello there im pleased to meet you.",
   sid: speakerId,
   speed: speed,
